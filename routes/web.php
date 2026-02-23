82517e150d365593f30d214c02e34435b7613b2e
kc calip <kristin.calip@cdsp.edu.ph>
2026-02-24 03:17:52 +0800

    Refactor routes to use controllers and resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\EnrollmentController;
+use App\Http\Controllers\FeeController;
+use App\Http\Controllers\GradeController;
+use App\Http\Controllers\AdminController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::resource('students', StudentController::class);
+Route::resource('enrollment', EnrollmentController::class)->only(['index','show']);
+Route::resource('fees', FeeController::class)->only(['index','create','store']);
+Route::resource('grades', GradeController::class)->only(['index','edit','update']);
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [AdminController::class, 'index'])->name('index');
 });
